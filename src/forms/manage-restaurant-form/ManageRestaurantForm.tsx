b409a9706e3f38400d09d4f8ef23936641af189d
ahsan-19 <112505556+ahsan-19@users.noreply.github.com>
2024-08-22 12:38:50 +0500

    created restaurant  page

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -5,6 +5,12 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import DetailsSection from './DetailsSections';
+import { Separator } from '@/components/ui/separator';
+import CuisinesSection from './CuisinesSection';
+import MenuSection from './MenuSection';
+import ImageSection from './ImageSection';
+import LoadingButton from '@/components/LoadingButton';
+import { Button } from '@/components/ui/button';
 
 const formSchema = z
     .object({
@@ -67,7 +73,36 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
     })
 
     const onSubmit = (formDataJson: RestaurantFormData) => {
+        //basically , before this we weere sending applicatoion/json data , but this time we want to send form data object 
+        const formData = new FormData();
+        formData.append("restaurantName", formDataJson.restaurantName);
+        formData.append("city", formDataJson.city);
+        formData.append("country", formDataJson.country);
 
+        formData.append(
+            "deliveryPrice",
+            (formDataJson.deliveryPrice * 100).toString()
+        );
+        formData.append(
+            "estimatedDeliveryTime",
+            formDataJson.estimatedDeliveryTime.toString()
+        );
+        formDataJson.cuisines.forEach((cuisine, index) => {
+            formData.append(`cuisines[${index}]`, cuisine);
+        });
+        formDataJson.menuItems.forEach((menuItem, index) => {
+            formData.append(`menuItems[${index}][name]`, menuItem.name);
+            formData.append(
+                `menuItems[${index}][price]`,
+                (menuItem.price * 100).toString()
+            );
+        });
+
+        if (formDataJson.imageFile) {
+            formData.append(`imageFile`, formDataJson.imageFile);
+        }
+
+        onSave(formData);
     }
     return (
         <Form {...form}>
@@ -75,6 +110,14 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
                 className='space-y-8 bg-gray-50 p-10 rounded-lg'
             >
                 <DetailsSection />
+                <Separator />
+                <CuisinesSection />
+                <Separator />
+                <MenuSection />
+                <Separator />
+                <ImageSection />
+                {isLoading ? <LoadingButton /> : <Button type='submit'>Submit</Button>}
+
             </form>
         </Form>
     )
