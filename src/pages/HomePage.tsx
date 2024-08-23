d5c96c36aaf4bbe0f014f35638f280f1f81574c7
ahsan-19 <112505556+ahsan-19@users.noreply.github.com>
2024-08-24 01:40:26 +0500

    search page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,14 +2,27 @@ import colors from '@/styles/theme'
 // import React from 'react'
 import landingPage from '@/assets/landing.png'
 import appDownloadImage from '@/assets/appDownload.png'
+import SearchBar, { SearchForm } from '@/components/SearchBar'
+import { useNavigate } from 'react-router-dom'
+
+
 const HomePage = () => {
+    const navigate = useNavigate();
+    const handleSearchSubmit = (searchFormValues: SearchForm) => {
+        navigate({
+          pathname: `/search/${searchFormValues.searchQuery}`,
+        });
+      };
     return (
         <div className='flex flex-col gap12'>
-            <div className='bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 -mt-16 text-center '>
+            <div className='md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 -mt-16 text-center '>
                 <h1 className={`text-5xl font-bold tracking-tight ${colors.mainTextColor}`}>
                     Have your takeaway right now
                 </h1>
                 <span className='text-xl'> Your fav food is just a click away</span>
+                <SearchBar placeHolder='Search by City' 
+                onSubmit={handleSearchSubmit}
+                />
             </div>
             <div className='grid md:grid-cols-2 gap-5'>
                 <img src={landingPage} alt=".." />
